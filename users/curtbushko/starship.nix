805154e94485aa239e11f90f3fdc4874a3e873d3
Curt Bushko <cbushko@gmail.com>
2024-02-24 13:07:31 -0500

    add hostname to starship if sshing

--- users/curtbushko/starship.nix
+++ users/curtbushko/starship.nix
@@ -4,11 +4,11 @@
         enableZshIntegration = true;
         settings = {
             add_newline = true;
-            format = "[░▒▓](#a3aed2)[  ](bg:#a3aed2 fg:#090c0c)[](bg:#769ff0 fg:#a3aed2)$directory[](fg:#769ff0 bg:#394260)$git_branch$git_status[](fg:#394260 bg:#212736)$golang[](fg:#212736 bg:#1d2230)$character";
+            format = "[░▒▓](#a3aed2)[  ](bg:#a3aed2 fg:#090c0c)$hostname[](bg:#769ff0 fg:#a3aed2)$directory[](fg:#769ff0 bg:#394260)$git_branch$git_status[](fg:#394260 bg:#212736)$golang[](fg:#212736 bg:#1d2230)$character";
             hostname = {
-                format = "[@$hostname]($style) ";
+                format = "[ @$hostname ]($style)";
                 ssh_only = true;
-                style = "fg:#e3e5e5 bg:#769ff0";
+                style = "bg:#a3aed2 fg:#090c0c";
             };
             directory = {
                 truncation_symbol = "…/";
