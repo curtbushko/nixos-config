a11747d8cf53c7c23c750d53652f85a6963d3f24
Curt Bushko <cbushko@gmail.com>
2024-07-02 16:12:22 -0400

    all: update to nix 24.05

--- modules/home/shells/zsh.nix
+++ modules/home/shells/zsh.nix
@@ -18,7 +18,7 @@
 }: {
   programs.zsh = {
     enable = true;
-    enableAutosuggestions = true;
+    autosuggestion.enable = true;
     enableCompletion = true;
     defaultKeymap = "viins";
     enableVteIntegration = true;
--- modules/home/wm/default.nix
+++ modules/home/wm/default.nix
@@ -37,7 +37,7 @@ in {
       slurp
       wl-clipboard
 
-      eww-wayland
+      eww
       swww
       swappy # snapshot tool
       swaybg
