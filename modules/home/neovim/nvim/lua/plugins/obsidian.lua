c3244e0be404e846b49bc4d2a0b4cf5c928793b3
Curt Bushko <cbushko@gmail.com>
2024-09-08 13:27:02 -0400

    nvim: cleanup obsidian plugin

--- modules/home/neovim/nvim/lua/plugins/obsidian.lua
+++ modules/home/neovim/nvim/lua/plugins/obsidian.lua
@@ -18,6 +18,50 @@ return {
           date_format = "%Y%m%d-%a",
           time_format = "%H:%M",
         },
+        preferred_link_style = "markdown",
+
+        ui = {
+          enable = true, -- set to false to disable all additional syntax features
+          update_debounce = 200, -- update delay after a text change (in milliseconds)
+          max_file_length = 5000, -- disable UI features for files with more than this many lines
+          -- Define how various check-boxes are displayed
+          checkboxes = {
+            -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+            [" "] = { char = "󰄱 ", hl_group = "ObsidianTodo" },
+            ["x"] = { char = " ", hl_group = "ObsidianDone" },
+            [">"] = { char = " ", hl_group = "ObsidianRightArrow" },
+            ["~"] = { char = "󰰱 ", hl_group = "ObsidianTilde" },
+            ["!"] = { char = " ", hl_group = "ObsidianImportant" },
+            -- Replace the above with this if you don't have a patched font:
+            -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+            -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+
+            -- You can also add more custom ones...
+          },
+          -- Use bullet marks for non-checkbox lists.
+          bullets = { char = "•", hl_group = "ObsidianBullet" },
+          external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+          -- Replace the above with this if you don't have a patched font:
+          -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+          reference_text = { hl_group = "ObsidianRefText" },
+          highlight_text = { hl_group = "ObsidianHighlightText" },
+          tags = { hl_group = "ObsidianTag" },
+          block_ids = { hl_group = "ObsidianBlockID" },
+          hl_groups = {
+            -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
+            ObsidianTodo = { bold = true, fg = "#f78c6c" },
+            ObsidianDone = { bold = true, fg = "#89ddff" },
+            ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
+            ObsidianTilde = { bold = true, fg = "#ff5370" },
+            ObsidianImportant = { bold = true, fg = "#d73128" },
+            ObsidianBullet = { bold = true, fg = "#89ddff" },
+            ObsidianRefText = { underline = true, fg = "#c792ea" },
+            ObsidianExtLinkIcon = { fg = "#c792ea" },
+            ObsidianTag = { italic = true, fg = "#89ddff" },
+            ObsidianBlockID = { italic = true, fg = "#89ddff" },
+            ObsidianHighlightText = { bg = "#75662e" },
+          },
+        },
       })
     end,
     -- :ObsidianBacklinks for getting a location list of references to the current buffer.
@@ -33,11 +77,11 @@ return {
     --
     keys = {
       { "<leader>Ob", "<CMD>ObsidianBacklinks<CR>", desc = "backlinks" },
-      { "<leader>Od", "<CMD>daily-note<CR>", desc = "new daily note" },
+      { "<leader>Od", "<CMD>ObsidianToday<CR>", desc = "new daily note" },
       { "<leader>Of", "gf", desc = "follow link" },
       { "<leader>Ol", "<CMD>ObsidianLink<CR>", desc = "link" },
       { "<leader>Om", "<CMD>ObsidianLinkNew<CR>", desc = "new note from link" },
-      { "<leader>On", "<CMD>new-note<CR>", desc = "new note" },
+      { "<leader>On", "<CMD>ObsidianNew<CR>", desc = "new note" },
       { "<leader>Oy", "<CMD>ObsidianYesterday<CR>", desc = "yesterdays daily note" },
     },
   },
