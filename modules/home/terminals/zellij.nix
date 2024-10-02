1017274c1fa92def866d004feb991af94c1fcdf8
Curt Bushko <cbushko@gmail.com>
2024-10-02 00:25:31 -0400

    all: add zellij bindings for new pane right(alt+n), new pane down(alt+m), toggle floating(alt+t), create floating(alt+p)

--- modules/home/terminals/zellij.nix
+++ modules/home/terminals/zellij.nix
@@ -1,23 +1,7 @@
 {
-  # Snowfall Lib provides a customized `lib` instance with access to your flake's library
-  # as well as the libraries available from your flake's inputs.
-  lib,
-  # An instance of `pkgs` with your overlays and packages applied is also available.
-  pkgs,
-  # You also have access to your flake's inputs.
-  inputs,
-  # Additional metadata is provided by Snowfall Lib.
-  system, # The system architecture for this host (eg. `x86_64-linux`).
-  target, # The Snowfall Lib target for this system (eg. `x86_64-iso`).
-  format, # A normalized name for the system target (eg. `iso`).
-  virtual, # A boolean to determine whether this system is a virtual target using nixos-generators.
-  systems, # An attribute map of your defined hosts.
-  # All other arguments come from the module system.
   config,
   ...
-}: let
-  plugin_zjstatus = inputs.zjstatus.outputs.packages.${pkgs.stdenv.hostPlatform.system}.default;
-in {
+}: {
   programs.zellij = {
     enable = true;
   };
@@ -34,81 +18,81 @@ in {
           //locked {
           //    bind "Ctrl g" { SwitchToMode "Normal"; }
           //}
-          resize {
-              bind "Alt r" { SwitchToMode "Normal"; }
-              bind "h" "Left" { Resize "Increase Left"; }
-              bind "j" "Down" { Resize "Increase Down"; }
-              bind "k" "Up" { Resize "Increase Up"; }
-              bind "l" "Right" { Resize "Increase Right"; }
-              bind "H" { Resize "Decrease Left"; }
-              bind "J" { Resize "Decrease Down"; }
-              bind "K" { Resize "Decrease Up"; }
-              bind "L" { Resize "Decrease Right"; }
-              bind "=" "+" { Resize "Increase"; }
-              bind "-" { Resize "Decrease"; }
-          }
-          pane {
-              bind "Alt p" { SwitchToMode "Normal"; }
-              bind "h" "Left" { MoveFocus "Left"; }
-              bind "l" "Right" { MoveFocus "Right"; }
-              bind "j" "Down" { MoveFocus "Down"; }
-              bind "k" "Up" { MoveFocus "Up"; }
-              bind "p" { SwitchFocus; }
-              bind "n" { NewPane; SwitchToMode "Normal"; }
-              bind "d" { NewPane "Down"; SwitchToMode "Normal"; }
-              bind "r" { NewPane "Right"; SwitchToMode "Normal"; }
-              bind "x" { CloseFocus; SwitchToMode "Normal"; }
-              bind "f" { ToggleFocusFullscreen; SwitchToMode "Normal"; }
-              bind "z" { TogglePaneFrames; SwitchToMode "Normal"; }
-              bind "w" { ToggleFloatingPanes; SwitchToMode "Normal"; }
-              bind "e" { TogglePaneEmbedOrFloating; SwitchToMode "Normal"; }
-              bind "c" { SwitchToMode "RenamePane"; PaneNameInput 0;}
-          }
-          move {
-              bind "Alt m" { SwitchToMode "Normal"; }
-              bind "n" "Tab" { MovePane; }
-              bind "p" { MovePaneBackwards; }
-             bind "h" "Left" { MovePane "Left"; }
-             bind "j" "Down" { MovePane "Down"; }
-              bind "k" "Up" { MovePane "Up"; }
-              bind "l" "Right" { MovePane "Right"; }
-          }
-          tab {
-              //bind "Alt t" { SwitchToMode "Normal"; }
-              bind "r" { SwitchToMode "RenameTab"; TabNameInput 0; }
-              bind "h" "Left" "Up" "k" { GoToPreviousTab; }
-              bind "l" "Right" "Down" "j" { GoToNextTab; }
-              bind "n" { NewTab; SwitchToMode "Normal"; }
-              bind "x" { CloseTab; SwitchToMode "Normal"; }
-              bind "s" { ToggleActiveSyncTab; SwitchToMode "Normal"; }
-              bind "b" { BreakPane; SwitchToMode "Normal"; }
-              bind "]" { BreakPaneRight; SwitchToMode "Normal"; }
-              bind "[" { BreakPaneLeft; SwitchToMode "Normal"; }
-              bind "1" { GoToTab 1; SwitchToMode "Normal"; }
-              bind "2" { GoToTab 2; SwitchToMode "Normal"; }
-              bind "3" { GoToTab 3; SwitchToMode "Normal"; }
-              bind "4" { GoToTab 4; SwitchToMode "Normal"; }
-              bind "5" { GoToTab 5; SwitchToMode "Normal"; }
-              bind "6" { GoToTab 6; SwitchToMode "Normal"; }
-              bind "7" { GoToTab 7; SwitchToMode "Normal"; }
-              bind "8" { GoToTab 8; SwitchToMode "Normal"; }
-              bind "9" { GoToTab 9; SwitchToMode "Normal"; }
-              //bind "Tab" { ToggleTab; }
-          }
-          scroll {
-              bind "Alt s" { SwitchToMode "Normal"; }
-              bind "e" { EditScrollback; SwitchToMode "Normal"; }
-              bind "s" { SwitchToMode "EnterSearch"; SearchInput 0; }
-              bind "Ctrl c" { ScrollToBottom; SwitchToMode "Normal"; }
-              bind "j" "Down" { ScrollDown; }
-              bind "k" "Up" { ScrollUp; }
-              bind "Ctrl f" "PageDown" "Right" "l" { PageScrollDown; }
-              bind "Ctrl b" "PageUp" "Left" "h" { PageScrollUp; }
-              bind "d" { HalfPageScrollDown; }
-              bind "u" { HalfPageScrollUp; }
-              // uncomment this and adjust key if using copy_on_select=false
-              // bind "Alt c" { Copy; }
-          }
+          // resize {
+          //     bind "Alt r" { SwitchToMode "Normal"; }
+          //     bind "h" "Left" { Resize "Increase Left"; }
+          //     bind "j" "Down" { Resize "Increase Down"; }
+          //     bind "k" "Up" { Resize "Increase Up"; }
+          //     bind "l" "Right" { Resize "Increase Right"; }
+          //     bind "H" { Resize "Decrease Left"; }
+          //     bind "J" { Resize "Decrease Down"; }
+          //     bind "K" { Resize "Decrease Up"; }
+          //     bind "L" { Resize "Decrease Right"; }
+          //     bind "=" "+" { Resize "Increase"; }
+          //     bind "-" { Resize "Decrease"; }
+          // }
+          // pane {
+          //     bind "Alt p" { SwitchToMode "Normal"; }
+          //     bind "h" "Left" { MoveFocus "Left"; }
+          //     bind "l" "Right" { MoveFocus "Right"; }
+          //     bind "j" "Down" { MoveFocus "Down"; }
+          //     bind "k" "Up" { MoveFocus "Up"; }
+          //     bind "p" { SwitchFocus; }
+          //     bind "n" { NewPane; SwitchToMode "Normal"; }
+          //     bind "d" { NewPane "Down"; SwitchToMode "Normal"; }
+          //     bind "r" { NewPane "Right"; SwitchToMode "Normal"; }
+          //     bind "x" { CloseFocus; SwitchToMode "Normal"; }
+          //     bind "f" { ToggleFocusFullscreen; SwitchToMode "Normal"; }
+          //     bind "z" { TogglePaneFrames; SwitchToMode "Normal"; }
+          //     bind "w" { ToggleFloatingPanes; SwitchToMode "Normal"; }
+          //     bind "e" { TogglePaneEmbedOrFloating; SwitchToMode "Normal"; }
+          //     bind "c" { SwitchToMode "RenamePane"; PaneNameInput 0;}
+          // }
+          // move {
+          //     bind "Alt m" { SwitchToMode "Normal"; }
+          //     bind "n" "Tab" { MovePane; }
+          //     bind "p" { MovePaneBackwards; }
+          //     bind "h" "Left" { MovePane "Left"; }
+          //     bind "j" "Down" { MovePane "Down"; }
+          //     bind "k" "Up" { MovePane "Up"; }
+          //     bind "l" "Right" { MovePane "Right"; }
+          // }
+          // tab {
+          //     //bind "Alt t" { SwitchToMode "Normal"; }
+          //     bind "r" { SwitchToMode "RenameTab"; TabNameInput 0; }
+          //     bind "h" "Left" "Up" "k" { GoToPreviousTab; }
+          //     bind "l" "Right" "Down" "j" { GoToNextTab; }
+          //     bind "n" { NewTab; SwitchToMode "Normal"; }
+          //     bind "x" { CloseTab; SwitchToMode "Normal"; }
+          //     bind "s" { ToggleActiveSyncTab; SwitchToMode "Normal"; }
+          //     bind "b" { BreakPane; SwitchToMode "Normal"; }
+          //     bind "]" { BreakPaneRight; SwitchToMode "Normal"; }
+          //     bind "[" { BreakPaneLeft; SwitchToMode "Normal"; }
+          //     bind "1" { GoToTab 1; SwitchToMode "Normal"; }
+          //     bind "2" { GoToTab 2; SwitchToMode "Normal"; }
+          //     bind "3" { GoToTab 3; SwitchToMode "Normal"; }
+          //     bind "4" { GoToTab 4; SwitchToMode "Normal"; }
+          //     bind "5" { GoToTab 5; SwitchToMode "Normal"; }
+          //     bind "6" { GoToTab 6; SwitchToMode "Normal"; }
+          //     bind "7" { GoToTab 7; SwitchToMode "Normal"; }
+          //     bind "8" { GoToTab 8; SwitchToMode "Normal"; }
+          //     bind "9" { GoToTab 9; SwitchToMode "Normal"; }
+          //     //bind "Tab" { ToggleTab; }
+          // }
+          // scroll {
+          //     bind "Alt s" { SwitchToMode "Normal"; }
+          //     bind "e" { EditScrollback; SwitchToMode "Normal"; }
+          //     bind "s" { SwitchToMode "EnterSearch"; SearchInput 0; }
+          //     bind "Ctrl c" { ScrollToBottom; SwitchToMode "Normal"; }
+          //     bind "j" "Down" { ScrollDown; }
+          //     bind "k" "Up" { ScrollUp; }
+          //     bind "Ctrl f" "PageDown" "Right" "l" { PageScrollDown; }
+          //     bind "Ctrl b" "PageUp" "Left" "h" { PageScrollUp; }
+          //     bind "d" { HalfPageScrollDown; }
+          //     bind "u" { HalfPageScrollUp; }
+          //     // uncomment this and adjust key if using copy_on_select=false
+          //     // bind "Alt c" { Copy; }
+          // }
           //search {
           //    bind "Alt s" { SwitchToMode "Normal"; }
           //    bind "Alt c" { ScrollToBottom; SwitchToMode "Normal"; }
@@ -174,7 +158,10 @@ in {
           shared_except "locked" {
           //    bind "Ctrl g" { SwitchToMode "Locked"; }
               //bind "Alt q" { Quit; }
-              bind "Alt n" { NewPane; }
+              bind "Alt n" { NewPane "Right"; }
+              bind "Alt m" { NewPane "Down"; }
+              bind "Alt p" { ToggleFloatingPanes; SwitchToMode "Normal"; }
+              bind "Alt t" { TogglePaneEmbedOrFloating; }
               bind "Alt h" "Alt Left" { MoveFocusOrTab "Left"; }
               bind "Alt l" "Alt Right" { MoveFocusOrTab "Right"; }
               bind "Alt j" "Alt Down" { MoveFocus "Down"; }
@@ -306,16 +293,16 @@ in {
       //
       //themes {
       //    tokyo-night-custom {
-      //    fg "#dfe9ff"
-      //     black "#15161e"
-      //     red "#f7768e"
-      //     green "#9ece6a"
-      //     yellow "#ffec6e"
-      //     blue "#7aa2f7"
-      //     magenta "#bb9af7"
-      //     cyan "#65bcff"
-      //     white "#dfe9ff"
-      //     orange "#ff9e64"
+      //    fg "//dfe9ff"
+      //     black "//15161e"
+      //     red "//f7768e"
+      //     green "//9ece6a"
+      //     yellow "//ffec6e"
+      //     blue "//7aa2f7"
+      //     magenta "//bb9af7"
+      //     cyan "//65bcff"
+      //     white "//dfe9ff"
+      //     orange "//ff9e64"
       //   }
       //}
       // themes {
