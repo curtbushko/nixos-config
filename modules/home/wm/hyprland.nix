8bd6fc313e87e77ccbf79c609706778473bb07cb
Curt Bushko <cbushko@gmail.com>
2024-10-11 17:07:11 -0400

    nixos: add some window resize keys

--- modules/home/wm/hyprland.nix
+++ modules/home/wm/hyprland.nix
@@ -1,19 +1,5 @@
 {
-  # Snowfall Lib provides a customized `lib` instance with access to your flake's library
-  # as well as the libraries available from your flake's inputs.
-  lib,
-  # An instance of `pkgs` with your overlays and packages applied is also available.
   pkgs,
-  # You also have access to your flake's inputs.
-  inputs,
-  # Additional metadata is provided by Snowfall Lib.
-  system, # The system architecture for this host (eg. `x86_64-linux`).
-  target, # The Snowfall Lib target for this system (eg. `x86_64-iso`).
-  format, # A normalized name for the system target (eg. `iso`).
-  virtual, # A boolean to determine whether this system is a virtual target using nixos-generators.
-  systems, # An attribute map of your defined hosts.
-  # All other arguments come from the module system.
-  config,
   ...
 }: let
   isLinux = pkgs.stdenv.isLinux;
@@ -200,7 +186,9 @@ in {
         "$super alt, p, pin"
 
         "$super $alt, h, resizeactive, -40 0"
+        "$super $alt, -, resizeactive, -40 0"
         "$super $alt, l, resizeactive, 40 0"
+        "$super $alt, =, resizeactive, 40 0"
         "$super $alt, k, resizeactive, 0 -40"
         "$super $alt, j, resizeactive, 0 40"
       ];
