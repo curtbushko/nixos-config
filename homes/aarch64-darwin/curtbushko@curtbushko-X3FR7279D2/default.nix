850eea595871c718a6b001b0d1ec3acd6489364e
Curt Bushko <cbushko@gmail.com>
2025-02-13 23:34:48 -0500

    add other mac

---
+++ homes/aarch64-darwin/curtbushko@curtbushko-X3FR7279D2/default.nix
@@ -0,0 +1,56 @@
+{
+  pkgs,
+  inputs,
+  system,
+  ...
+}: {
+  home.enableNixpkgsReleaseCheck = false;
+  home.stateVersion = "18.09";
+
+  # Let home manager manage itself
+  programs.home-manager.enable = true;
+  xdg.enable = true;
+
+  #---------------------------------------------------------------------
+  # Home Options
+  #---------------------------------------------------------------------
+  curtbushko = {
+    git.enable = true;
+    k8s.enable = true;
+    programming.enable = true;
+    secrets.enable = true;
+    shells.enable = true;
+    terminals.enable = true;
+    tools.enable = true;
+    theme = {
+      name = "tokyo-night-neon";
+      wallpaper = "my-city-gx.jpg";
+    };
+
+  };
+
+  #---------------------------------------------------------------------
+  # Packages
+  #---------------------------------------------------------------------
+  home.packages = [
+    # Darwin only
+    pkgs.cachix
+    inputs.neovim.packages.${system}.default
+  ];
+
+  #---------------------------------------------------------------------
+  # Env vars and dotfiles
+  #---------------------------------------------------------------------
+  home.sessionVariables = {
+    LANG = "en_US.UTF-8";
+    LC_CTYPE = "en_US.UTF-8";
+    LC_ALL = "en_US.UTF-8";
+    EDITOR = "nvim";
+    PAGER = "less -FirSwX";
+    TERM = "xterm-256color";
+  };
+
+  imports = [
+    inputs.stylix.homeManagerModules.stylix
+  ];
+}
