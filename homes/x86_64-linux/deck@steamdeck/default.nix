e2f6a06fbbf83941731aa37dfd58d9798f39f4e0
Curt Bushko <cbushko@gmail.com>
2025-12-28 18:00:49 -0500

    add steamdeck host

---
+++ homes/x86_64-linux/deck@steamdeck/default.nix
@@ -0,0 +1,63 @@
+{
+  # An instance of `pkgs` with your overlays and packages applied is also available.
+  pkgs,
+  # You also have access to your flake's inputs.
+  inputs,
+  ...
+}: {
+  home.stateVersion = "18.09";
+  home.enableNixpkgsReleaseCheck = false;
+
+  # Let home manager manage itself
+  programs.home-manager.enable = true;
+
+  xdg.enable = true;
+
+  #---------------------------------------------------------------------
+  # Home Options
+  #---------------------------------------------------------------------
+  curtbushko = {
+    browsers.enable = true;
+    gaming.enable = true;
+    git.enable = true;
+    programming.enable = true;
+    secrets.enable = true;
+    shells.enable = true;
+    terminals.enable = true;
+    tools.enable = true;
+    wm = {
+      tools.enable = true;
+      niri.enable = true;
+      rofi.enable = false;
+    };
+    theme = {
+      name = "gruvbox-material";
+      wallpaper = "cyberpunk-three.png";
+    };
+  };
+
+  #---------------------------------------------------------------------
+  # Packages
+  #---------------------------------------------------------------------
+  home.packages = [
+    pkgs.cachix
+    pkgs.tailscale
+    inputs.neovim.packages.${pkgs.stdenv.hostPlatform.system}.default
+  ];
+
+  #---------------------------------------------------------------------
+  # Env vars and dotfiles
+  #---------------------------------------------------------------------
+  home.sessionVariables = {
+    LANG = "en_US.UTF-8";
+    LC_CTYPE = "en_US.UTF-8";
+    LC_ALL = "en_US.UTF-8";
+    EDITOR = "nvim";
+    PAGER = "less -FirSwX";
+    TERM = "xterm-ghostty";
+  };
+
+  imports = [
+    inputs.stylix.homeModules.stylix
+  ];
+}
