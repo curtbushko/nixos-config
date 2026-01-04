d2edb873d978c4acafca9979566edaada3b7ae02
Curt Bushko <cbushko@gmail.com>
2026-01-04 17:06:12 -0500

    performance: wallpapers need SHAs. No idea why MacOS didnt fail with this

--- modules/home/wm/niri.nix
+++ modules/home/wm/niri.nix
@@ -8,8 +8,19 @@
   inherit (lib) types mkOption mkIf;
   cfg = config.curtbushko.wm.niri;
   colors = lib.importJSON ../styles/${config.curtbushko.theme.name}.json;
-  wallpaper = builtins.fetchurl {
+
+  # Wallpaper SHA256 hashes
+  wallpaperHashes = {
+    "firewatch-green.jpg" = "sha256-ILiIAcw9JMHsRNT63TnR3kn1O4IwEliJtb2FwdIQUEM=";
+    "cyberpunk-tokyo.png" = lib.fakeHash;
+    "cyberpunk_2077_phantom_liberty_katana.jpg" = lib.fakeHash;
+    "green-pasture.jpg" = lib.fakeHash;
+    "cyberpunk-three.png" = lib.fakeHash;
+  };
+
+  wallpaper = pkgs.fetchurl {
     url = "https://raw.githubusercontent.com/curtbushko/nixos-wallpapers/main/3440x1440/${config.curtbushko.theme.wallpaper}";
+    sha256 = wallpaperHashes.${config.curtbushko.theme.wallpaper} or lib.fakeHash;
   };
 in {
   options.curtbushko.wm.niri = {
--- modules/home/wm/swaybg.nix
+++ modules/home/wm/swaybg.nix
@@ -7,8 +7,19 @@
   inherit (lib) mkIf;
   cfg = config.curtbushko.wm.niri;
   isLinux = pkgs.stdenv.isLinux;
-  wallpaper = builtins.fetchurl {
+
+  # Wallpaper SHA256 hashes
+  wallpaperHashes = {
+    "firewatch-green.jpg" = "sha256-ILiIAcw9JMHsRNT63TnR3kn1O4IwEliJtb2FwdIQUEM=";
+    "cyberpunk-tokyo.png" = lib.fakeHash;
+    "cyberpunk_2077_phantom_liberty_katana.jpg" = lib.fakeHash;
+    "green-pasture.jpg" = lib.fakeHash;
+    "cyberpunk-three.png" = lib.fakeHash;
+  };
+
+  wallpaper = pkgs.fetchurl {
     url = "https://raw.githubusercontent.com/curtbushko/nixos-wallpapers/main/3440x1440/${config.curtbushko.theme.wallpaper}";
+    sha256 = wallpaperHashes.${config.curtbushko.theme.wallpaper} or lib.fakeHash;
   };
 in {
   config = mkIf cfg.enable {
